5e56c5f1ec9f78ecbaa0807058c88c481bac4365
Wasabee69 <130854883+Wasabee69@users.noreply.github.com>
2025-09-16 20:26:23 +0200

    Update Stable_matching_problem_Gale_Shapley.py

--- Stable_matching_problem_Gale_Shapley.py
+++ Stable_matching_problem_Gale_Shapley.py
@@ -1,53 +1,54 @@
-from collections import deque as deque
-#N = int(input())
+from collections import deque
 
+def invert(pref, N):
+    d = [[-1] * N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            d[i][pref[i][j]] = j
+    return d
 
-N = 4
-pref_man = [
+def define_stable_matching(pref_man, pref_women):
+    N = len(pref_man)
+    pref_women = invert(pref_women, N)
+    husband = {}
+    queue = deque(range(N))
+    next_proposal = [0] * N
+
+    while queue:
+        m = queue.popleft()
+        w = pref_man[m][next_proposal[m]]
+        next_proposal[m] += 1
+
+        if w not in husband:
+            husband[w] = m
+        else:
+            m2 = husband[w]
+            # check if woman prefers new man old man (m2)
+            if pref_women[w][m] < pref_women[w][m2]:
+                queue.append(m2)
+                husband[w] = m
+            else:
+                queue.append(m)
+    return husband
+
+stable_match = define_stable_matching(
+    pref_man = [
     [3, 1, 2, 0],
     [1, 0, 2, 3],
     [0, 1, 2, 3],
     [0, 1, 2, 3]
-]
+],
 pref_women = [
     [0, 1, 2, 3],
     [0, 1, 2, 3],
     [0, 1, 2, 3],
     [0, 1, 2, 3]
 ]
-def inverse(pref):
-    d = {i:{} for i in range(N)}
-    for i in range(N):
-        for j in range(N):
-            d[i][pref[i][j]] = j
-    return d
-
-pref_man = inverse(pref_man)
-pref_women = inverse(pref_women)
-husband = {}
-queue = deque(list(range(N)))
-next_proposal = [0] * N
-
-while queue:
-    m = queue.popleft()
-    w = pref_man[m][next_proposal[m]]
-    next_proposal[m] += 1
-
-    if w not in husband:
-        # velger første partner
-        husband[w] = m
-    else:
-        m2 = husband[w]
-        if pref_women[w][m2] < pref_women[w][m]:
-            #bytter partner
-            queue.append(m2)
-            husband[w] = m
-        else:
-            queue.append(m)
-
-print(husband)
-print([(husband[w], w) for w in range(N)])
+)
+print(stable_match)
+print([(stable_match[w], w) for w in range(len(stable_match))])
 
 
 
         
+
